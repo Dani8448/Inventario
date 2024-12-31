c0bb930f5575db55617faf0c34b58a95f39926e4
Dani8448 <dalx8448@gmail.com>
2024-12-31 12:01:47 +0100

    Creación de la clase Producto

---
+++ producto.py
@@ -0,0 +1,8 @@
+class Producto:
+    def __init__(self, nombre, cantidad, precio):
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def __str__(self):
+        return f"{self.nombre}: {self.cantidad} unidades a {self.precio}€ cada una"
